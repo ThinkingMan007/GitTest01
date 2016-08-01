00bf8170eee19d5ac59802c7bf58698f73353e4c
luozi_007 <moualong163@163.com>
2016-08-02 00:44:42 +0800

    normal

--- src/com/atguigu/git/test02/GitTest02.java
+++ src/com/atguigu/git/test02/GitTest02.java
@@ -4,7 +4,7 @@ public class GitTest02 {
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub");
 		System.out.println("Hello GitHub2");
-			System.out.println("Hello GitHub222222");
-				System.out.println("Hello GitHub2222222");
+		System.out.println("Hello GitHub222222");
+		System.out.println("Hello GitHub2222222");
 	}
 }
