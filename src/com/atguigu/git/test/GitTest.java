98cdcb19064f76333c6f226f686160fbe32f29ad
luozi_007 <moualong163@163.com>
2016-08-02 00:58:59 +0800

    111

--- src/com/atguigu/git/test/GitTest.java
+++ src/com/atguigu/git/test/GitTest.java
@@ -3,5 +3,7 @@ package com.atguigu.git.test;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("git test 01");
+		System.out.println("git test 0133333");
+		System.out.println("git test 0144444");
 	}
 }
